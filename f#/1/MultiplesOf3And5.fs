5efb37dc77beff3706d98715abe23c27da0bb3a8
nsv <volsci@tuta.io>
2019-01-24 08:38:02 +1000

    Update f#/1/MultiplesOf3And5.fs
    Complete Problem 1 in F#

---
+++ f#/1/MultiplesOf3And5.fs
@@ -0,0 +1,11 @@
+﻿open System
+
+let multiplesOf3And5() =
+
+    List.filter (fun x -> (x % 3 = 0) || (x % 5 = 0)) [1..999]
+    |> List.sum
+    |> printfn "%A"     
+
+multiplesOf3And5()
+
+Console.ReadKey() |> ignore
