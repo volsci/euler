15bffe67956c94abeaabc8a96a0307c935514997
nsv <volsci@tuta.io>
2019-01-29 09:08:54 +1000

    Create EvenFibonacciNumbers.fs
    Complete Problem 2 in F#

---
+++ f#/2/EvenFibonacciNumbers.fs
@@ -0,0 +1,10 @@
+﻿open System
+
+Seq.unfold (fun state ->
+    if (snd state > 4000000) then None
+    else Some(fst state + snd state, (snd state, fst state + snd state))) (0,1)
+|> Seq.where (fun x -> x % 2 = 0)
+|> Seq.sum
+|> printfn "%A" 
+
+Console.ReadKey() |> ignore
