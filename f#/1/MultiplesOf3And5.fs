bbb5eb8f6f4d29a6b964be4af12c8aac4e6b99b0
nsv <volsci@tuta.io>
2019-01-29 19:53:08 +1000

    Remove Boilerplate

--- f#/1/MultiplesOf3And5.fs
+++ f#/1/MultiplesOf3And5.fs
@@ -1,11 +1,7 @@
 ﻿open System
 
-let multiplesOf3And5() =
-
-    List.filter (fun x -> (x % 3 = 0) || (x % 5 = 0)) [1..999]
-    |> List.sum
-    |> printfn "%A"     
-
-multiplesOf3And5()
+List.filter (fun x -> (x % 3 = 0) || (x % 5 = 0)) [1..999]
+|> List.sum
+|> printfn "%A"     
 
 Console.ReadKey() |> ignore
